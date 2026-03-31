1491b37de913a67c558bcf7953cb93efdc507a46
morgan thorne <morgangthorne@outlook.com>
2026-03-31 19:37:19 -0400

    Added a getter test to main

--- M3. Introduction to Classes Date Class.cpp
+++ M3. Introduction to Classes Date Class.cpp
@@ -17,38 +17,13 @@ int main()
 
     //Temp Object 
     Date TestDate;
-    /*
-    cout << TestDate.NumberString() << endl;
-
-    Date ValidDate(12, 25, 2021);
-    cout << ValidDate.NumberString() << endl;
-
-    Date InvalidDate(2, 30, 2021);
-    cout << InvalidDate.NumberString() << endl;
-
-    Date d1(5, 10, 2022);
-    cout << d1.NumberString() << endl;
-
-    //Invalid temp
-    d1.SetDate(2, 30, 2022);
-    cout << d1.NumberString() << endl;
-    */
-    /*
-    Date d1(12, 25, 2021);
-    cout << d1.NumberString() << endl;
-    cout << d1.LongString() << endl;
-    cout << d1.InternString() << endl;
-    */
-
-    cout << "Constructor Test\n";
-    Date d1; 
-    cout << d1.NumberString() << endl;
     
-    Date d2(12, 25, 2021); 
-    cout << d2.NumberString() << endl;
+    cout << "getter test\n";
 
-    Date d3(2, 30, 2021);
-    cout << d3.NumberString() << endl; //Should be invalid
+    Date g1(7, 4, 2022);
+    cout << g1.GetMonth() << endl;
+    cout << g1.GetDay() << endl;
+    cout << g1.GetYear() << endl;
 
     cout << "Program complete\n";
 
