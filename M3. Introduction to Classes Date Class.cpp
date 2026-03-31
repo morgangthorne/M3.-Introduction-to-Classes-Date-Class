275ec5aea372fc378c44d4948584b4f1681c9b4a
morgan thorne <morgangthorne@outlook.com>
2026-03-31 16:20:33 -0400

    Add project files.

---
+++ M3. Introduction to Classes Date Class.cpp
@@ -0,0 +1,12 @@
+// M3. Introduction to Classes Date Class.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
